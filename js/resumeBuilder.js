7f37b70f67490c737f6952ddfd39871c7860aa06
Andres Rojas <andres.rojas@mtnsat.com>
2015-03-07 17:09:43 -0500

    Add work info

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -51,3 +51,30 @@ var education = {
   ],
   display: function() {}
 };
+
+var work = {
+  jobs: [
+    {
+      employer: "MTN Satellite Communications",
+      title: "DevOps Engineer",
+      location: "Miramar, FL",
+      dates: "2012 - Present",
+      description: "<ul><li>Automated deployment of various PaaS platforms (i.e. Stackato, Cloud Foundry, AppFog) on to 20 seven-node bare metal clusters, and three multi-node AWS clusters</li><li>Developed custom automation tools using Ruby, Bash, Chef & Razor</li><li>Set up and administered AWS services, including EC2, VPC, S3, Route 53 & RDS</li><li>First place in the company's first annual Hackathon</li></ul>"
+    },
+    {
+      employer: "MTN Satellite Communications",
+      title: "Systems Development Technician II",
+      location: "Miramar, FL",
+      dates: "2011 - 2012",
+      description: "<ul><li>Developed & deployed custom Linux & Windows-based kiosks</li><li>Assisted with the development of PBX administration tools</li><li>Administed hardware & software firewalls & routers</li><li>Conducted site surveys to deploy WLAN aboard cruise ships</li></ul>"
+    },
+    {
+      employer: "MTN Satellite Communications",
+      title: "R&D Support Technician",
+      location: "Miramar, FL",
+      dates: "2007 - 2011",
+      description: "<ul><li>Assisted in the development of Windows-based kiosks</li><li>Provided remote technical support for clients at sea</li><li>Developed and deployed a LAMP-based internal wiki</li></ul>"
+    }
+  ],
+  display: function() {}
+};
