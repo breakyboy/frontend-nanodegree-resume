ef0a885f0ad7e551a2f55f62afa3fb493e5ba764
Andres Rojas <andres.rojas@mtnsat.com>
2015-03-07 15:53:01 -0500

    Add education info

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -17,3 +17,37 @@ var bio = {
   biopic: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/p/7/000/231/2af/029918f.jpg",
   display: function() {}
 };
+
+var education = {
+  schools: [
+    {
+      name: "Miami Dade College",
+      location: "North Miami, FL",
+      degree: "Associate of Arts",
+      majors: [
+        "Focus in Computer Science"
+      ],
+      dates: 2009,
+      url: "http://www.mdc.edu/"
+    },
+    {
+      name: "Florida International University",
+      location: "Miami, FL",
+      degree: "Bachelor of Science",
+      majors: [
+        "Computer Science"
+      ],
+      dates: 0,   // Interpret as 'Coursework not completed'
+      url: "http://www.fiu.edu/"
+    }
+  ],
+  onlineCourses: [
+    {
+      title: "Front-End Web Developer Nanodegree",
+      school: "Udacity",
+      date: 0,   // Interpret as 'Coursework not completed'
+      url: "https://www.udacity.com/course/nd001"
+    }
+  ],
+  display: function() {}
+};
