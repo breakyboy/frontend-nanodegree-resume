812fbdc722e8fdff7419c6d8d726ef3388ef2756
Andres Rojas <andres.rojas@mtnsat.com>
2015-03-07 15:38:04 -0500

    Add bio info

---
+++ js/resumeBuilder.js
@@ -0,0 +1,19 @@
+var bio = {
+  name: "Andres Rojas",
+  role: "DevOps Engineer",
+  contacts: {
+    mobile: "+1 305-982-7325",
+    email: "andres@consumingpatience.com",
+    github: "andres-rojas",
+    twitter: "breakyboy",
+    location: "Miramar"
+  },
+  welcomeMessage: "lorem ipsum dolor sit amet etc etc etc",
+  skills: [
+    "Ruby Developer",
+    "Cloud Infrastructure Automation",
+    "Continuous Integration / Continuous Deployment"
+  ],
+  biopic: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/p/7/000/231/2af/029918f.jpg",
+  display: function() {}
+};
